a19064338a6dc0bffc417f69b0ae9d187a089ec4
JosethCal18 <stevencalcor04@gmail.com>
2023-03-22 20:36:29 -0500

    Envio proyecto terminado

--- Calculadora.js
+++ Calculadora.js
@@ -1,49 +1,162 @@
-//ELEMENTOS INVOCADOS
-const var1 = document.querySelector('#valor1');
-const var2 = document.querySelector('#valor2');
-const btn = document.querySelector('#botonCalculo');
-const pResult = document.querySelector('#result');
-//ELEMENTOS INVOCADOS
+//BOARD ELEMENT
+const boardNumber = document.querySelector('.board');
+//BOARD ELEMENT
 
-//EVENTOS
-btn.addEventListener('click', operacion);
-//EVENTOS
+//NUMBER ELEMENTS
+const numberZero = document.getElementById('zero');
+const number1 = document.getElementById('one');
+const number2 = document.getElementById('two');
+const number3 = document.getElementById('three');
+const number4 = document.getElementById('four');
+const number5 = document.getElementById('five');
+const number6 = document.getElementById('six');
+const number7 = document.getElementById('seven');
+const number8 = document.getElementById('eight');
+const number9 = document.getElementById('nine');
+const commaKey = document.getElementById('comma');
+const pointKey = document.getElementById('point');
+//NUMBER ELEMENTS
 
-//FUNCIONES
-function operacion(){
-    const seleccionUsuario = Number(prompt('Seleccione el tipo de operacion que quiera realizar:\n 1.SUMA\n 2.RESTA\n 3.DIVISION\n 4.MULTIPLICACION'));
+//OPERATORS ELEMENTS
+const operatorPlus = document.getElementById('plus');
+const operatorSubtract = document.getElementById('subtract');
+const operatorDivision = document.getElementById('division');
+const operatorEquals = document.getElementById('equal');
+const operatorMultiplication = document.getElementById('multiplication');
+//OPERATORS ELEMENTS
 
-    if(seleccionUsuario == 1){
-        const num1 = Number(var1.value);
-        const num2 = Number(var2.value);
-        const result = (num1 + num2);
+//VARIABLES
+let placeBoard = boardNumber;
+let result;
+//VARIABLES
 
-        pResult.innerText = "Resultado: " + result;
-        return;
-    } else if(seleccionUsuario == 2){
-        const num1 = Number(var1.value);
-        const num2 = Number(var2.value);
-        const result = (num1 - num2);
+//EVENTS OPERATORS
+operatorPlus.addEventListener('click', listenPlus);
+operatorSubtract.addEventListener('click', listenSubtract);
+operatorDivision.addEventListener('click', listenDivision);
+operatorEquals.addEventListener('click', sendResult);
+operatorMultiplication.addEventListener('click', listenMultiplication);
+//EVENTS OPERATORS
 
-        pResult.innerText = "Resultado: " + result;
-        return;
-    } else if(seleccionUsuario == 3){
-        const num1 = Number(var1.value);
-        const num2 = Number(var2.value);
-        const result = (num1 / num2);
+//EVENTS NUMBERS
+numberZero.addEventListener('click', listenNumberZero);
+number1.addEventListener('click', listenNumberOne);
+number2.addEventListener('click', listenNumberTwo);
+number3.addEventListener('click', listenNumberThree);
+number4.addEventListener('click', listenNumberFour);
+number5.addEventListener('click', listenNumberFive);
+number6.addEventListener('click', listenNumberSix);
+number7.addEventListener('click', listenNumberSeven);
+number8.addEventListener('click', listenNumberEight);
+number9.addEventListener('click', listenNumberNine);
+commaKey.addEventListener('click', listenNumberComma);
+pointKey.addEventListener('click', listenNumberPoint);
+//EVENTS NUMBERS
 
-        pResult.innerText = "Resultado: " + result;
-        return;
-    } else if(seleccionUsuario == 4){
-        const num1 = Number(var1.value);
-        const num2 = Number(var2.value);
-        const result = (num1 * num2);
-
-        pResult.innerText = "Resultado: " + result;
-        return;
-    } else{
-        alert('Ingresa alguno de los numeros indicados');
-        operacion();
+//FUNCTIONS NUMBERS
+function listenNumberZero(){
+    if(numberZero.click){
+        placeBoard.innerText += 0;
         return
+    };
+}
+function listenNumberOne(){
+    if(numberZero.click){
+        placeBoard.innerText += 1;
+        return
+    };
+}
+function listenNumberTwo(){
+    if(number2.click){
+        placeBoard.innerText += 2;
+        return
+    };
+}
+function listenNumberThree(){
+    if(number3.click){
+        placeBoard.innerText += 3;
+        return
+    };
+}
+function listenNumberFour(){
+    if(number4.click){
+        placeBoard.innerText += 4;
+        return
+    };
+}
+function listenNumberFive(){
+    if(number5.click){
+        placeBoard.innerText += 5;
+        return
+    };
+}
+function listenNumberSix(){
+    if(number6.click){
+        placeBoard.innerText += 6;
+        return
+    };
+}
+function listenNumberSeven(){
+    if(number7.click){
+        placeBoard.innerText += 7;
+        return
+    };
+}
+function listenNumberEight(){
+    if(number8.click){
+        placeBoard.innerText += 8;
+        return
+    };
+}
+function listenNumberNine(){
+    if(number9.click){
+        placeBoard.innerText += 9;
+        return
+    };
+}
+function listenNumberComma(){
+    if(commaKey.click){
+        placeBoard.innerText += ',';
+        return
+    };
+}
+function listenNumberPoint(){
+    if(pointKey.click){
+        placeBoard.innerText += '.';
+        return
+    };
+}
+//FUNCTIONS NUMBERS
+
+//FUNCTIONS OPERATORS
+function listenPlus(){
+    if(operatorPlus.click){
+        placeBoard.innerText += '+';
+        return
+    };
+}
+function listenSubtract(){
+    if(operatorSubtract.click){
+        placeBoard.innerText += '-';
+        return
+    };
+}
+function listenDivision(){
+    if(operatorDivision.click){
+        placeBoard.innerText += '/';
+        return
+    };
+}
+function listenMultiplication(){
+    if(operatorMultiplication.click){
+        placeBoard.innerText += '*';
+        return
+    };
+}
+function sendResult(){
+    if(operatorEquals.click){
+        result = eval(boardNumber.innerText);
+        boardNumber.innerText = result;
     }
 }
+//FUNCTIONS OPERATORS
